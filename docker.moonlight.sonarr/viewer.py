d3dd734ab222e30b113663d4f57f15e65254f79d
Dalen <dalencattmlsp@gmail.com>
2021-11-22 17:39:59 -0800

    trying to fix a naming conflict

---
+++ docker.moonlight.sonarr/viewer.py
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+import xbmcaddon
+import xbmc
+
+__addon__ = xbmcaddon.Addon();
+
+xbmc.log('Launching Viewer: ' + __addon__.getSetting('HOMEPAGE'))
+xbmc.executebuiltin('RunAddon("script.kiosk.browser", ' + __addon__.getSetting('HOMEPAGE') + ')')
+
+del __addon__
